5a82c3df473757a87cf84a4c69de8996daa53e1b
Piotr <posinsk@gmail.com>
2025-07-23 15:37:59 +0200

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { ApiClient, FetchHttpClientAdapter } from './client';
+export { generateOpenApiSpec } from './openapi'
 export { CreateApiDefinition, CreateResponses, ApiDefinitionSchema } from './definition';
 export { RegisterHandlers, EndpointMiddleware, UniversalEndpointMiddleware, SimpleMiddleware, EndpointInfo } from './object-handlers';
 export { File as UploadedFile } from './router';
